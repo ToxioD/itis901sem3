fff7e804c20da509bf4d8e8489eb45fc533b3e78
Lysenkov Dmitriy <lysenkovds@gmail.com>
2020-12-15 22:59:28 +0300

    Added timer to Setup screen

--- semester_works/semwork2/src/main/java/ru/itis/controllers/SetupController.java
+++ semester_works/semwork2/src/main/java/ru/itis/controllers/SetupController.java
@@ -1,22 +1,32 @@
 package ru.itis.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import ru.itis.utils.ScreenNavigator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SetupController implements Initializable {
 
     private Stage stage;
+    private ScheduledExecutorService service;
 
     @FXML
     private Pane buffPane;
@@ -51,9 +61,12 @@ public class SetupController implements Initializable {
     @FXML
     private Button submitButton;
 
+    @FXML
+    private ProgressBar timerBar;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        submitButton.setOnAction(event -> ScreenNavigator.loadScreen(ScreenNavigator.WAIT));
+        submitButton.setOnAction(event -> ScreenNavigator.loadScreen(ScreenNavigator.CHAT));
 
         //test button press
         refreshButton.setOnAction(event -> System.out.println("Refresh button pressed"));
@@ -67,6 +80,16 @@ public class SetupController implements Initializable {
 
         //test value insertion
         goldLabel.setText("5");
+
+        service = Executors.newScheduledThreadPool(1);
+        service.schedule(() -> Platform.runLater(() -> ScreenNavigator.loadScreen(ScreenNavigator.WAIT)),
+                30, TimeUnit.SECONDS);
+
+        Timeline timer = new Timeline(new KeyFrame(Duration.millis(30), animation -> {
+            timerBar.setProgress(timerBar.getProgress() - 0.001d);
+        }));
+        timer.setCycleCount(999);
+        timer.play();
     }
 
     private void choose(Boolean isBuff) {
